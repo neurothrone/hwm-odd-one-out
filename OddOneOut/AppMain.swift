afdb828540786d82ebd6a9878fef90968db98c3a
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-09 05:01:12 +0100

    add assets

---
+++ OddOneOut/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  OddOneOut
+//
+//  Created by Zaid Neurothrone on 2022-12-09.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
